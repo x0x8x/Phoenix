b02742d9b44379ff1953ef82dba154a240df2a6c
x0x8x <5467132+x0x8x@users.noreply.github.com>
2020-05-01 22:37:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import logging
+import pyrogram_mod
+from pyrogram import Client
+
+client = Client("Phoenix")
+
+cmds = {}
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARN)
+logging.getLogger("pyrogram").setLevel(logging.WARN)
+LOGS = logging.getLogger(__name__)
