39df186cfc1da48e86b250884f9181b52c56bdac
x0x8x <5467132+x0x8x@users.noreply.github.com>
2020-05-01 22:37:42 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import config
+import asyncio
+import sys
+import os
+
+
+async def run_client(client):
+    try:
+        await client.start()
+    except AttributeError as e:
+        return print(
+            str(e).split(". ")[0]
+            + f". Run '{os.path.basename(sys.executable)} setup.py' first."
+        )
+    client.set_parse_mode("combined")
+
+    await client.idle()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_client(config.client))
